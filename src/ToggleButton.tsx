797ce8798df10f056f9877b0aa1f28218fdd59ff
hassaan-0316 <hassaanather00@gmail.com>
2024-10-28 13:28:05 +0500

    some changes in radio and togglebutton

--- src/ToggleButton.tsx
+++ src/ToggleButton.tsx
@@ -1,18 +1,19 @@
 import React, { memo } from 'react';
-import { ViewProps, Pressable, I18nManager } from 'react-native';
+import { ViewProps, I18nManager, ViewStyle } from 'react-native';
 import Animated, { useAnimatedStyle, interpolate, withTiming } from 'react-native-reanimated';
-import Block from './Block';
 import { iViewStyles } from './interface';
 import { colorPalletes } from './Constants';
+import TouchableBlock from './TouchableBlock';
 
 interface Props extends iViewStyles, ViewProps {
   handleToggle: () => void;
   value: boolean;
   disabled?: boolean;
+  innerBallStyles?: ViewStyle
 }
 
 const ToggleButton: React.FC<Props> = (props) => {
-  const { handleToggle, value, disabled } = props;
+  const { handleToggle, value, disabled, innerBallStyles } = props;
 
   const animatedStyle = useAnimatedStyle(() => {
     const translateX = interpolate(value ? 1 : 0, [0, 1], [0, I18nManager.isRTL ? -20 : 20]);
@@ -22,30 +23,30 @@ const ToggleButton: React.FC<Props> = (props) => {
   });
 
   return (
-    <Pressable
+    <TouchableBlock
       disabled={disabled}
+      onPress={handleToggle}
       accessible={true}
-      onPress={handleToggle}>
-      <Block
-        height={24}
-        width={44}
-        backgroundColor={value ? colorPalletes['alAdaam-80'] : colorPalletes['grey-60']}
-        borderRadius={40}
-        paddingHorizontal={2}
-        paddingVertical={2}>
-        <Animated.View
-          style={[
-            {
-              width: 20,
-              height: 20,
-              backgroundColor: 'white',
-              borderRadius: 40,
-            },
-            animatedStyle,
-          ]}
-        />
-      </Block>
-    </Pressable>
+      height={24}
+      width={44}
+      backgroundColor={value ? colorPalletes['alAdaam-80'] : colorPalletes['grey-60']}
+      borderRadius={40}
+      paddingHorizontal={2}
+      paddingVertical={2}
+      {...props}
+    >
+      <Animated.View
+        style={[
+          {
+            width: 20,
+            height: 20,
+            backgroundColor: 'white',
+            borderRadius: 40,
+          },
+          animatedStyle, innerBallStyles
+        ]}
+      />
+    </TouchableBlock>
   );
 };
 
