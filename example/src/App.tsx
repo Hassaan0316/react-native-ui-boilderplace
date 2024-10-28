e404487f0d35ad76092eee5faec7d8e68bd63021
hassaan-0316 <hassaanather00@gmail.com>
2024-10-28 13:26:46 +0500

    some changes in radio and togglebutton

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-import { multiply, Block, TextComponent } from 'react-native-ui-boilderplace-test';
+import { multiply, Block, TextComponent, RadioButton, ToggleButton } from 'react-native-ui-boilderplace-test';
 
 export default function App() {
   const [result] = useState<number | undefined>();
+  const [radio, setRadio] = useState<boolean>(false);
 
   useEffect(() => {
     multiply(3, 7)
@@ -12,6 +13,15 @@ export default function App() {
   return (
     <Block style={styles.container}>
       <TextComponent>Result: {result}</TextComponent>
+      <RadioButton
+        enable={radio}
+        handleChange={() => setRadio(prev => !prev)}
+      />
+
+      <ToggleButton
+        value={radio}
+        handleToggle={() => setRadio(prev => !prev)}
+      />
     </Block>
   );
 }
--- src/RadioButton.tsx
+++ src/RadioButton.tsx
@@ -1,41 +1,45 @@
-import { GestureResponderEvent, Pressable, ViewProps } from 'react-native';
+import { GestureResponderEvent, ViewProps, ViewStyle } from 'react-native';
 import { iViewStyles } from './interface';
 import Block from './Block';
 import { colorPalletes } from './Constants';
+import TouchableBlock from './TouchableBlock';
 
 interface iProps extends iViewStyles, ViewProps {
-  handleChange: ((event: GestureResponderEvent) => void) | null | undefined;
+  handleChange: ((event: GestureResponderEvent) => void) | undefined;
   enable: boolean;
   disabled?: boolean;
+  bubbleStyles?: ViewStyle;
+  size?: number;
+  ballSize?: number;
 }
 
 const RadioButton = (props: iProps) => {
-  const { enable, handleChange, disabled } = props;
+  const { enable, handleChange, disabled, bubbleStyles, size, ballSize} = props;
+  let SIZE = size || 20;
+  let BALL_SIZE = ballSize || 14;
 
   return (
-    <Pressable
+    <TouchableBlock
       disabled={disabled}
-      accessible={false}
-      importantForAccessibility="no-hide-descendants"
-      accessibilityElementsHidden
-      style={{ height: 24 }}
-      onPress={handleChange}>
+      width={SIZE}
+      height={SIZE}
+      backgroundColor={colorPalletes.white}
+      borderWidth={1}
+      borderColor={enable ? colorPalletes['alAdaam-80'] : colorPalletes['grey-40']}
+      borderRadius={SIZE / 2}
+      justify="center"
+      align="center"
+      onPress={handleChange}
+      {...props}
+    >
       <Block
-        width={20}
-        height={20}
-        backgroundColor={colorPalletes.white}
-        borderWidth={1}
-        borderColor={enable ? colorPalletes['alAdaam-80'] : colorPalletes['grey-40']}
-        borderRadius={10}
-        justify="center"
-        align="center"
-        {...props}>
-        <Block
-          width={14}
-          height={14}
-          backgroundColor={enable ? colorPalletes['alAdaam-80'] : colorPalletes.white}></Block>
-      </Block>
-    </Pressable>
+        width={BALL_SIZE}
+        height={BALL_SIZE}
+        borderRadius={BALL_SIZE / 2}
+        backgroundColor={enable ? colorPalletes['alAdaam-80'] : colorPalletes.white}
+        {...bubbleStyles}
+      />
+    </TouchableBlock>
   );
 };
 
